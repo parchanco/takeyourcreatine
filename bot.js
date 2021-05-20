7370c26b90f8c7db3c16cc8a0cc93329db07dffe
Pablo Archanco <pabloarchanco@gmail.com>
2021-05-20 21:17:20 +0200

    [LOAD]

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Twitter = require('twitter')
+require('dotenv').config()
+
+var T = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN,
+});
+
+var postMsg = () => {
+  T.post('statuses/update', { status: "Take the creatine" }, function (err, data, response) {
+    console.log(data)
+  });
+};
+
+postMsg();
