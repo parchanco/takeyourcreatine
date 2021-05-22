95ff3836aef0790d1604bac1e620c02b658ef573
Pablo Archanco <pabloarchanco@gmail.com>
2021-05-22 11:16:18 +0200

    [UPDATE] Auth method to 1.0 becaus 2.0 not able to post twitts? wtf

--- bot.js
+++ bot.js
@@ -4,15 +4,14 @@ require('dotenv').config()
 var T = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  bearer_token: process.env.TWITTER_BEARER_TOKEN,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-
 var postMsg = () => {
   T.post('statuses/update', { status: "Take the creatine" }, function (err, data, response) {
     console.log(data)
   });
 };
-console.log(postMsg)
 
-setInterval(function () { postMsg(); }, 60000 * 60 * 24)
+postMsg();
 
